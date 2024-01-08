4793c6e7c40a0b005c66fd6d7a09a5f7bca15d07
PalakGupta <PalakGupta@LININDAUG23-266.bbrouter>
2024-01-08 11:58:45 +0530

    pull

---
+++ Test Suites/Companion/Companion_Speak.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Companion_Speak</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>fe5d977c-c00e-4221-bb58-fa3e6868f84e</testSuiteGuid>
+</TestSuiteEntity>
